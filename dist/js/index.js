e6c18e0907dd3007f19c12b24be39b56e7b65c7b
Youssef Shaaban <youssef@gmail.com>
2020-09-01 22:54:47 +0200

    Creative Landing Page With An Image Gallery

---
+++ dist/js/index.js
@@ -0,0 +1,8 @@
+// Slider
+const slider = e => {
+    document.querySelector('.slider img').src = e
+}
+// Menu Toggle
+const toggle = () => {
+    document.querySelector('header nav ul').classList.toggle('show')
+}
